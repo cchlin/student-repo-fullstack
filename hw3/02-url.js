c58b0d487ecc5ded9fc6948bcfb9bd54e3c214be
Cheng Hsuan Lin <chlin@pdx.edu>
2022-10-25 23:23:15 -0700

    example 1-3 completed

--- hw3/02-url.js
+++ hw3/02-url.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const { URLSearchParams } = require('url');
 const port = process.env.PORT || 5001;
 
 const server = http.createServer((req, res) => {
@@ -32,6 +33,27 @@ const server = http.createServer((req, res) => {
   }
 
   // Add your code here
+  else if (req.url === '/attributes?hello=world&lorem=ipsum') {
+    result();
+  } else if (req.url === '/items?first=1&second=2&third=3&fourth=4') {
+    result();
+  } else if (
+    req.url === '/characters?spongebob=squarepants&patrick=star&sandy=cheeks'
+  ) {
+    result();
+  }
+
+  function result() {
+    // search parameters and creat the table and store them in a string
+    const searchParams = new URLSearchParams(url.searchParams);
+    let content = '<table border="1">';
+    searchParams.forEach((value, key) => {
+      content += '<tr><td>' + key + '</td><td>' + value + '</td></tr>';
+    });
+    content += '</table>';
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(content);
+  }
 
   res.end();
 });
